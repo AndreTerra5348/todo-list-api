aaa2ddc1f64bef2a07310d54005c41bf6ffc7119
André Terra <and.an.terra@gmail.com>
2022-05-12 14:59:15 -0300

    Remove scaffold files.

--- TodoList.Api/Startup.cs
+++ TodoList.Api/Startup.cs
@@ -21,14 +21,13 @@ namespace TodoList.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -48,7 +47,6 @@ namespace TodoList.Api
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
